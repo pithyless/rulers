9f3c0f1215c614d8532afc1aa1f505caf16ea90d
Norbert Wojtowicz <wojtowicz.norbert@gmail.com>
2012-09-16 18:08:48 +0200

    Add Controller#controller_name

--- lib/rulers/controller.rb
+++ lib/rulers/controller.rb
@@ -7,5 +7,10 @@ module Rulers
 
     attr_reader :env
 
+    def controller_name
+      name = self.class.to_s
+      name.gsub!(/Controller$/, '')
+      Inflector.underscore(name)
+    end
   end
 end
