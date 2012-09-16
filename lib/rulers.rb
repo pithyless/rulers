dc53a8bab2c4d8e765cd820d0e6e964b5e5567e7
Norbert Wojtowicz <wojtowicz.norbert@gmail.com>
2012-09-16 14:54:23 +0200

    Add most basic rack app

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -1,2 +1,11 @@
 module Rulers
+  class Application
+    def call(env)
+      [
+        200,
+        {'Content-Type' => 'text/html'},
+        ['Hello from your favorite framework!']
+      ]
+    end
+  end
 end
